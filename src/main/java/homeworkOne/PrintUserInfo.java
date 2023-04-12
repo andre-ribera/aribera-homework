6e4a67fde4fb05adfb69c569dc66c6041828626f
AjjRibera <110746567+GashaKasha@users.noreply.github.com>
2023-04-12 08:40:08 -0700

    implemented string.format

--- src/main/java/homeworkOne/PrintUserInfo.java
+++ src/main/java/homeworkOne/PrintUserInfo.java
@@ -2,8 +2,7 @@ package homeworkOne;
 
 public class PrintUserInfo {
     public static String printUserInfo(String name, int age, String homeCity){
-        String userInfo = "Hello! My name is " + name + ". I am " + age +
-                " years old and I live in the city of " + homeCity + ".";
+        String userInfo = String.format("Hello! My name is %s. I am %d years old and I live in the city of %s", name, age, homeCity);
         return userInfo;
     }
 }
